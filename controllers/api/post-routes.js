ca7ba2dec313512ca0ec8b7e4eac077c1e72bdf1
Gavin Murphy <gavinhmurphy@gmail.com>
2021-11-01 20:50:49 -0700

    post routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -6,7 +6,27 @@ router.route('/').get(async (req, res) => {
   try {
     const posts = await Post.findAll({
       include: [
-        { model: User, attributes: ['id', 'username'] },
-        { model: Comment, attributes: ['id', 'comment', 'user_id', 'date'] },
+        { model: User, 
+            attributes: ['id', 'username'] 
+        },
+        { model: Comment, 
+            attributes: ['id', 'comment', 'user_id', 'date'] 
+        },
       ],
     });
+    router
+    .route('/:id')
+    .get(async (req, res) => {
+      try {
+        const post = await Post.findByPk(req.params.id, {
+          include: [
+            { model: User, attributes: ['id', 'name'] },
+            { model: Comment, attributes: ['id', 'comment', 'user_id'] },
+          ],
+        });
+
+        res.status(200).json(post);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    })
--- models/Comment.js
+++ models/Comment.js
@@ -16,7 +16,7 @@ User.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    name: {
+    content: {
       type: DataTypes.STRING,
       allowNull: false,
     },
